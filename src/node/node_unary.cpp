716455a8e002ea0b78cd7924690c8dcc54d610a3
koikotya <koikoutya@gmail.com>
2024-03-26 02:00:28 +0900

    コンストラクタを実装(unary)

--- src/node/node_unary.cpp
+++ src/node/node_unary.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include <string>
 
+#include "attribute.hpp"
 #include "node.hpp"
 #include "node_unary.hpp"
 
 using std::ostream;
 using std::string;
 
+Unary::Unary(std::initializer_list<Node*> list) : Node(list) {
+    attr_ = childs_[0]->attr_;
+    attr_.sign = true;
+    if (attr_.type == UINT_TYPE) attr_.type = INT_TYPE;
+}
+
+UnaryPlus::UnaryPlus(std::initializer_list<Node*> list) : Unary(list) {
+}
+
 ostream& UnaryPlus::print(ostream& os,int i) const {
     os << string(4*i,' ') << "UnaryPlus ";
     return Node::print(os,i);
+}
+
+UnaryMinus::UnaryMinus(std::initializer_list<Node*> list) : Unary(list) {
+    attr_.negative ^= true;
 }
 
 ostream& UnaryMinus::print(ostream& os,int i) const {
--- src/node/node_unary.hpp
+++ src/node/node_unary.hpp
@@ -1,20 +1,32 @@
 #ifndef INCLUDED_UNARY
 #define INCLUDED_UNARY
 
+#include "attribute.hpp"
 #include "node.hpp"
 
+using std::initializer_list;
+
 class Unary : public Node {
+public:
     using Node::Node;
+
+    Unary(initializer_list<Node*> list) ;
 };
 
 class UnaryPlus : public Unary {
+public:
     using Unary::Unary;
+
+    UnaryPlus(initializer_list<Node*> list) ;
 
     ostream& print(ostream& os,int i = 0) const override;
 };
 
 class UnaryMinus : public Unary {
+public:
     using Unary::Unary;
+
+    UnaryMinus(initializer_list<Node*> list) ;
 
     ostream& print(ostream& os,int i = 0) const override;
 };
